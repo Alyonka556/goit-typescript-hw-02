30c59fc8dd62944f59dc8ba7f6ec2652b487e25b
Alyonka556 <142902721+Alyonka556@users.noreply.github.com>
2024-01-04 16:47:18 +0100

    h-w-typeScript-2

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,20 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum DayOfWeek {
+  MONDAY,
+  THUSDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+  SUNDAY,
+}
+
+function isWeekend(day: DayOfWeek): boolean {
+  return day === DayOfWeek.SATURDAY || day === DayOfWeek.SUNDAY;
+}
+
+// console.log(isWeekend(DayOfWeek.SATURDAY));
+// console.log(isWeekend(DayOfWeek.MONDAY));
--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,11 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<TKey, TValue> {
+  key: TKey;
+  value: TValue;
 }
+
+// const pair1: KeyValuePair<number, string> = { key: 1, value: "One" };
 
 export {};
